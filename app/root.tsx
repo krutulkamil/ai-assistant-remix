6e6ac0ca2c74c83346a57e9fc51af52ba9677c00
krutulkamil <krutulkamil@onet.pl>
2023-01-19 15:14:43 +0100

    feat: tailwind setup with remix

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,40 @@
-import type { MetaFunction } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration
 } from "@remix-run/react";
+import styles from "./styles/app.css";
+import type { FunctionComponent } from "react";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
-
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
+const App: FunctionComponent = (): JSX.Element => {
+    return (
+        <html lang="en">
+        <head>
+            <Meta />
+            <Links />
+        </head>
+        <body>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
+};
+
+export const meta: MetaFunction = () => ({
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1"
+});
+
+export const links: LinksFunction = () => {
+    return [{ rel: "stylesheet", href: styles }];
 }
+
+export default App;
